8739ef3ac955c002d96ce2127511f7c10ac3016f
bjrunning <senzhapov.02@mail.ru>
2023-09-24 20:59:15 +0300

    add app/src/main/java/hexlet/code/schemes/NumberSchema.java

--- app/src/main/java/hexlet/code/schemes/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemes/NumberSchema.java
@@ -1,4 +1,30 @@
 package hexlet.code.schemes;
 
-public class NumberSchema {
+import java.util.function.Predicate;
+
+public class NumberSchema extends BaseSchema {
+    {
+        init = o -> o == null || o instanceof Number;
+        addRequirement(init);
+    }
+    @Override
+    public void required() {
+        Predicate<Object> newReq = o -> o instanceof Integer;
+        removeInitReq();
+        addRequirement(newReq);
+    }
+
+    public void positive() {
+        Predicate<Object> newReq = o -> o == null || (Integer) o > 0;
+        addRequirement(newReq);
+    }
+
+    public void range(int n1, int n2) {
+        Predicate<Object> newReq = o -> isInRange(n1, n2, (Integer) o);
+        addRequirement(newReq);
+    }
+
+    private boolean isInRange(int floor, int ceil, int target) {
+        return target >= floor && target <= ceil;
+    }
 }
