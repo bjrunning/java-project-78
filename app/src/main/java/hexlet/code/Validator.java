bb5f7f438816679dfad7f6c00516441aa50fd7d3
bjrunning <senzhapov.02@mail.ru>
2023-09-24 20:58:34 +0300

    add app/src/main/java/hexlet/code/Validator.java

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,9 +1,13 @@
 package hexlet.code;
 
 import hexlet.code.schemes.StringSchema;
+import hexlet.code.schemes.NumberSchema;
 
 public final class Validator {
     public StringSchema string() {
         return new StringSchema();
+    }
+    public NumberSchema number() {
+        return new NumberSchema();
     }
 }
