0e8ba903c9e988f058e8cd510c9bca9c3173b94e
bjrunning <senzhapov.02@mail.ru>
2023-09-24 20:19:33 +0300

    add app/src/main/java/hexlet/code/schemes/Schema.java

--- app/src/main/java/hexlet/code/schemes/Schema.java
+++ app/src/main/java/hexlet/code/schemes/Schema.java
@@ -1,4 +1,28 @@
 package hexlet.code.schemes;
 
-public class Schema {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public abstract class Schema {
+    private final List<Predicate<Object>> requirements = new ArrayList<>();
+
+    abstract void required();
+
+    public boolean isValid(Object obj) {
+        for (Predicate<Object> req : requirements) {
+            if (!req.test(obj)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected void addRequirement(Predicate<Object> req) {
+        requirements.add(req);
+    }
+
+    protected void removeAllPredicates() {
+        requirements.clear();
+    }
 }
