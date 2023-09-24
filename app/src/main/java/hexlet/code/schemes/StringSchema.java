f1da2b810dab2b1ab8597514921d466814b807b3
bjrunning <senzhapov.02@mail.ru>
2023-09-24 20:19:47 +0300

    add app/src/main/java/hexlet/code/schemes/StringSchema.java

--- app/src/main/java/hexlet/code/schemes/StringSchema.java
+++ app/src/main/java/hexlet/code/schemes/StringSchema.java
@@ -1,2 +1,26 @@
-package hexlet.code.schemes;public class StringSchema {
+package hexlet.code.schemes;
+
+import java.util.function.Predicate;
+
+public class StringSchema extends Schema {
+    {
+        Predicate<Object> stringValidation = o -> o == null || o instanceof String && ((String) o).isEmpty();
+        addRequirement(stringValidation);
+    }
+    @Override
+    public void required() {
+        Predicate<Object> newReq = o -> o instanceof String && !((String) o).isEmpty();
+        removeAllPredicates();
+        addRequirement(newReq);
+    }
+
+    public void contains(String str) {
+        Predicate<Object> newReq = o -> ((String) o).contains(str);
+        addRequirement(newReq);
+    }
+
+    public void minLength(int num) {
+        Predicate<Object> newReq = o -> o.toString().length() >= num;
+        addRequirement(newReq);
+    }
 }
