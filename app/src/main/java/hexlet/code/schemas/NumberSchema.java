d32c87e6e896099bbaebe382fab846fe3698fefb
bjrunning <senzhapov.02@mail.ru>
2023-09-25 16:41:36 +0300

    add app/src/main/java/hexlet/code/schemas/NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -2,26 +2,28 @@ package hexlet.code.schemas;
 
 import java.util.function.Predicate;
 
-public class NumberSchema extends BaseSchema {
-    {
-        init = o -> o == null || o instanceof Number;
-        addRequirement(init);
-    }
-    @Override
-    public void required() {
-        Predicate<Object> newReq = o -> o instanceof Integer;
-        removeInitReq();
-        addRequirement(newReq);
+public final class NumberSchema extends BaseSchema {
+
+    public NumberSchema() {
+        Predicate<Object> init = o -> o instanceof Number;
+        addRequirement(SchemaName.INITIAL, init);
     }
 
-    public void positive() {
-        Predicate<Object> newReq = o -> o == null || (Integer) o > 0;
-        addRequirement(newReq);
+    public NumberSchema required() {
+        required = true;
+        return this;
     }
 
-    public void range(int n1, int n2) {
+    public NumberSchema positive() {
+        Predicate<Object> newReq = o -> (Integer) o > 0;
+        addRequirement(SchemaName.POSITIVE, newReq);
+        return this;
+    }
+
+    public NumberSchema range(int n1, int n2) {
         Predicate<Object> newReq = o -> isInRange(n1, n2, (Integer) o);
-        addRequirement(newReq);
+        addRequirement(SchemaName.RANGE, newReq);
+        return this;
     }
 
     private boolean isInRange(int floor, int ceil, int target) {
