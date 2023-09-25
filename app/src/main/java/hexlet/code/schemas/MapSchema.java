b54e9d83d29822e8e95d5cacdf3e10f22bc7805d
bjrunning <senzhapov.02@mail.ru>
2023-09-25 14:55:26 +0300

    add app/src/main/java/hexlet/code/schemas/MapSchema.java

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,4 +1,24 @@
 package hexlet.code.schemas;
 
-public class MapSchema {
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class MapSchema extends BaseSchema {
+    Predicate<Object> currentSizePredicate;
+    {
+        init = o -> o == null || o instanceof Number;
+        addRequirement(init);
+    }
+    @Override
+    public void required() {
+        Predicate<Object> newReq = o -> o instanceof Map;
+        removeInitReq();
+        addRequirement(newReq);
+    }
+
+    public void sizeof(int n) {
+        removeSpecifiedReq(currentSizePredicate);
+        currentSizePredicate = o -> o instanceof Map && ((Map<?, ?>) o).size() == n;
+        addRequirement(currentSizePredicate);
+    }
 }
